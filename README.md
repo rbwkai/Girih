22693ac64a20430c9f8a24580a474cdec438cede
Kabir <mahiulkabir@outlook.com>
2025-03-06 17:09:55 +0600

    readme update

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ### Girih
 ---
 ***The Geometry Engine***
+---
+
+To run the project,
+clone the repo; cd into that;
+
+```
+mkdir build
+cd build
+mkdir render
+cmake ..
+cmake --build .
+./Girih
+```
+
--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,19 @@ int main(){
   node ori;
   node r_start(200, 0);
 
-  rotor r1(200, ori);
+  rotor r1(256, ori);
+  rotor r2(128, r1);
+  rotor r3(64, r2);
 
   proxim d1(r_start, r1); 
 
   vector<keyframe<float>> keys = {
-    keyframe(r1.arg, (float)0, pi),
+    keyframe(r1.arg, (float)0, 4*pi),
+    keyframe(r2.arg, (float)0, 2*pi),
+    keyframe(r3.arg, (float)0, -8*pi),
   };
   
   cout<<"distance from start: "<<(d1.len())<<endl;
-  animate(r1, cnv, keys);
+  animate(r3, cnv, keys);
   cout<<"distance from start: "<<(d1.len())<<endl;
 }
