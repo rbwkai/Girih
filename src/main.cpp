14b4e1db56e3ff1339db4162aeabce63e7203a2d
Kabir <mahiulkabir@outlook.com>
2025-03-07 17:40:28 +0600

    smoooth colorator

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,9 @@ int main(){
   node ori;
   node r_start(200, 0);
 
-  rotor r1(256, ori); Keyframe kf1(r1.arg, (float)0, 4*pi);
+  rotor r1(250, ori); Keyframe kf1(r1.arg, (float)0, 2*pi);
+  rotor r2(180, r1); Keyframe kf2(r2.arg, (float)0, -4*pi);
 
-  rotor r2(128, r1); Keyframe kf2(r2.arg, (float)0, 2*pi);
-
-  rotor r3(64, r2); Keyframe kf3(r3.arg, (float)0, -8*pi);
 
   proxim d1(r_start, r1); 
 
@@ -26,10 +24,10 @@ int main(){
   RGBA pen; Keyframe kfc(pen, red, blue);
 
   vector<Keyframe> keys = {
-    kf1, kf2, kf3, kfc
+    kf1, kf2, kfc
   };
   
   cout<<"distance from start: "<<(d1.len())<<endl;
-  animate(r3, cnv, pen, keys, 0);
+  animate(r2, cnv, pen, keys, 0);
   cout<<"distance from start: "<<(d1.len())<<endl;
 }
