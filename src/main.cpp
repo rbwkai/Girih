2da021794d5240175d55a7a39110f3f615ad5c87
Nayeem10 <nayeemhossain2110@gmail.com>
2025-03-08 14:53:50 +0600

    Added list features

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,46 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 
+#include <numbers>
+
+#include "./anim/animate.hpp"
 #include "./object/obj.hpp"
 #include "./param/parameter.hpp"
-#include "./anim/animate.hpp"
-#include <numbers>
 
 using namespace std;
 
 float pi = 3.1415927410125732421875;
 
-int main(){
+int main() {
   canvas cnv(1024);
 
-  Node ori(0, 0);
-  Rotor r1(300, ori); Keyframe kf1(r1.arg, float(0), 4 * pi);
-  Rotor r2(50, r1, Red);   Keyframe kf2(r2.arg, float(0), 9 * pi);
-  Rotor r3(150, ori); Keyframe kf3(r3.arg, float(0), 4 * pi);
-  Rotor r4(50, r3);   Keyframe kf4(r4.arg, float(0), 8 * pi);
-  Circle cir(r1, float(50), Lavender);
-  Keyframe kf5(cir.radius, float(50), float(100));
-  segment seg1(r1, r2), seg2(r3, r4);
+  Node ori(-200, 0);
 
-  Keyframe kfc1(cir.color, Blue, Red);
-  // Keyframe kfc2()
-  vector<Keyframe*> keys = {
-    &kf1, &kf2, &kf3, &kf4, &kf5, &kfc1
-  };
-  vector<Point*> objList = {
-    &r1, &r2, &r3, &r4
-  };
-  
-  vector<segment*> segList = {
-    &seg1, &seg2
-  };
-    
-  vector<Line*> lineList = {
-    
-  };
+  HorizontalLine x_axis(ori);
+  cnv.draw_line(&x_axis, White, 1);
 
+  VerticalLine y_axis(ori);
+  cnv.draw_line(&y_axis, White, 1);
+
+  Rotor r1(120, ori, Maroon);
+  Keyframe kf_r1(r1.arg, float(0), 2 * pi);
+
+  segment sr(ori, r1);
+
+  Node tm(0, 0);
+  Keyframe kf_tm(tm.x, float(0), float(350));
+  HorizontalLine H(r1);
+  VerticalLine V(tm);
+
+  IntersectionPoint I(H, V, Red);
+  segment seg(I, tm, Sapphire), seg2(I, r1, Rosewater);
+
+  Keyframe kfc(I.color, Red, Blue);
+  vector<Keyframe*> keys = {&kf_r1, &kf_tm, &kfc};
+  vector<Point*> objList = {&r1, &I};
+  vector<segment*> segList = {&sr, &seg, &seg2};
+  vector<Line*> lineList = {};
   vector<Circle*> circleList = {
-    &cir
+
   };
 
   animate(objList, segList, lineList, circleList, cnv, keys, 0);
