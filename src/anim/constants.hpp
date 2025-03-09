d6447595ac0e3946fb6e05869a7388b51580af66
Nayeem10 <nayeemhossain2110@gmail.com>
2025-03-09 21:42:51 +0600

    Text allignment fixed. Special character added.

--- src/anim/constants.hpp
+++ src/anim/constants.hpp
@@ -3,7 +3,7 @@
 
 namespace Constant {
 constexpr int FRAME_RATE = 30;
-constexpr float DURATION = 5.0;
+constexpr float DURATION = 3.0;
 constexpr int STEPS_PER_FRAME = 1200;  // point density: more the better
 constexpr int TOTAL_STEPS = FRAME_RATE * DURATION * STEPS_PER_FRAME;
 }  // namespace Constant
